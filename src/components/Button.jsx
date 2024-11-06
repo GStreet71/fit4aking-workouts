3f9340dc6b32c133c571a61f02d1e280333b4e42
Gregory H <gharrissr.kc@gmail.com>
2024-11-06 10:00:14 -0600

    Updated styling.
    Increase the range of thebackground gradient. Apply updated gradient it to the buttons.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Button(props) {
     const { text, func } = props
     return (
-        <button onClick={func} className='px-8 mx-auto py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200'>
+        <button onClick={func} className='blue-shadow px-8 mx-auto py-4 rounded-md border-[2px] bg-gradient-to-r 
+      from-slate-700 to-slate-950 border-blue-400 border-solid duration-200'>
             <p>{text}</p>
         </button>
     )
